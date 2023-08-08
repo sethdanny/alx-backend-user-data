95a6b13d235ac078c882e7da347128c09589dfdf
sethdanny <naddulidaniel1994@gmail.com>
2023-08-08 12:45:05 +0300

    made changes in index

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> str:
+def unauthorized() -> None:
     """ GET /api/v1/unauthorized
     Return:
         - Unauthorized error
